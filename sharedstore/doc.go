6c0416c22f5a0ebfb2510159a3593aa2f0ad970c
Sebastien Lavoie <sebastien.lavoie@shopify.com>
2021-08-18 10:39:08 -0400

    Remove sharedstore
    Migrated to https://github.com/Shopify/go-sharedstore in an effort to
    reduce the large list of dependencies in the goose repo

--- sharedstore/doc.go
+++
@@ -1,5 +0,0 @@
-// Package sharedstore provides shared storage where only one thread (local or on another instance) works on preparing the data for a key.
-//
-// While another Store instance has the lock, the store will poll periodically until it’s unlocked.
-// To reduce the stress on the Client, other threads of the local store will wait for the polling thread to complete.
-package sharedstore
